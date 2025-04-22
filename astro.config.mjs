34b74c9e9b5b81430e360ea134a6ee2098d0d664
3nvi <agg.arvanitakis@gmail.com>
2025-04-22 13:33:58 +0300

    chore: update Astro configuration to use Node.js adapter conditionally and set trailing slash behavior

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,23 @@ import preact from '@astrojs/preact';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import netlify from '@astrojs/netlify';
+import node from '@astrojs/node';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aggelos.dev',
-  adapter: netlify(),
+  trailingSlash: 'never',
+
+  // Use Netlify adapter if running in Netlify build environment, otherwise use Node.js adapter.
+  adapter: process.env.NETLIFY === 'true' ? netlify() : node({ mode: 'standalone' }),
 
   integrations: [
     preact({ compat: true }),
     mdx(),
     partytown({ config: { forward: ['dataLayer.push'] } }),
   ],
-
-  experimental: {
-    svg: true,
-    session: true,
-  },
 
   vite: {
     plugins: [tailwindcss()],
